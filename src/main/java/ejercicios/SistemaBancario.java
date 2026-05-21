e90d97938ff3a40e108ed64965a27d14b93b7782
Manuel <manuelverko95@gmail.com>
2026-05-21 12:57:06 -0400

    feat: ejercicio clase abstracta CuentasBancarias
    Implementa CuentaAhorro con interés porcentual y CuentaCorriente con comisión fija. Polimorfismo aplicado desde ArrayList<CuentasBancarias>.

---
+++ src/main/java/ejercicios/SistemaBancario.java
@@ -0,0 +1,99 @@
+package main.java.ejercicios;
+
+import java.util.ArrayList;
+
+enum TipoRentabilidad{
+    DIARIA, MENSUAL, ANUAL
+}
+
+abstract class CuentasBancarias{
+    protected String nombreCuenta;
+    protected int numeroCuenta;
+    protected double saldo;
+
+    public CuentasBancarias(String nombreCuenta, int numeroCuenta, double saldo) {
+        this.nombreCuenta = nombreCuenta;
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+    }
+    public abstract void presentarse();
+    public abstract void mostrarSaldo();
+    public abstract double calcularIntereses();
+}
+
+class CuentaAhorro extends CuentasBancarias{
+    static final double INTERES = 0.0125;
+    TipoRentabilidad rentabilidad;
+
+    public CuentaAhorro(String nombre, int numeroCuenta, double saldo, TipoRentabilidad rentabilidad) {
+        super(nombre, numeroCuenta, saldo);
+        this.rentabilidad = rentabilidad;
+    }
+
+    public void presentarse(){
+        System.out.printf("Nombre cuenta: %s \nNumero cuenta: %d\nSaldo: %f\nTipo Rentabilidad:%s", nombreCuenta, numeroCuenta, saldo, rentabilidad);
+    }
+
+    @Override
+    public void mostrarSaldo(){
+        System.out.println(super.saldo);
+    }
+
+    @Override
+    public double calcularIntereses(){
+        /* Sería interesante que en el futuro hicieramos refactor y que añadamos fecha creacion a la cuenta
+        y que simulemos que pasen días, meses, años, y que calculemos el interes acumulado.
+        deberemos implementar una función de calcular días transcurridos, y determinar cierres de día, de mes, y de años
+        para que corran sobre la composición del interes sobre el saldo.
+       */
+        return super.saldo * INTERES;
+    }
+}
+
+class CuentaCorriente extends CuentasBancarias{
+    static final double COMISION_MENSUAL = 7500;
+
+    public CuentaCorriente(String nombreCuenta, int numeroCuenta, double saldo) {
+        super(nombreCuenta, numeroCuenta, saldo);
+    }
+
+    public void presentarse(){
+        System.out.printf("Nombre cuenta: %s \nNumero cuenta: %d\nSaldo: %f", nombreCuenta, numeroCuenta, saldo);
+    }
+
+    @Override
+    public void mostrarSaldo(){
+        System.out.println(super.saldo);
+    }
+    @Override
+    public double calcularIntereses(){
+
+        return COMISION_MENSUAL;
+    }
+
+}
+
+public class SistemaBancario {
+    public static void main(String[] args) {
+        ArrayList<CuentasBancarias> cuentas = new ArrayList<>();
+
+
+        CuentaAhorro nc = new CuentaAhorro(
+                "Cuenta ahorro maxima",
+                23241,
+                1500000,
+                TipoRentabilidad.DIARIA
+        );
+       CuentaCorriente cc = new CuentaCorriente(
+               "Cuenta corriente común",
+               114543434,
+               800000
+       );
+        cuentas.add(nc);
+        cuentas.add(cc);
+        for(CuentasBancarias cb: cuentas){
+            cb.presentarse();
+            System.out.println("\n");
+        }
+    }
+}
